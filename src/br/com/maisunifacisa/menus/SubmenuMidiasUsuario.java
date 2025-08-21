9935e93d69ff4a6ce2ad953f496d5aa20a7b4439
Ana Karolyne <anakarolyne.oa@gmail.com>
2025-08-21 20:23:18 -0300

    Criação inicial do submenuMidiasUsuario

--- src/br/com/maisunifacisa/menus/SubmenuMidiasUsuario.java
+++ src/br/com/maisunifacisa/menus/SubmenuMidiasUsuario.java
@@ -4,8 +4,35 @@ import java.util.Scanner;
 
 public class SubmenuMidiasUsuario {
     public void exibirSubmenuMidias(Scanner sc){
-        System.out.println("Eu sou o submenu de midias");
+        int opcao = -1;
 
+        while(opcao !=3){
+            System.out.println("1 - Adicionar Mídia");
+            System.out.println("2 - Remover Mídia");
+            System.out.println("3 - Voltar para o Menu de Usuários");
+            System.out.print("Digite uma Opção: ");
+            opcao = sc.nextInt();
+            sc.nextLine();
+
+            switch (opcao){
+                case 1:
+                    System.out.println("Adicinando mídia");
+                    break;
+                case 2:
+                    System.out.println("Removendo mídias");
+                    break;
+                case 3:
+                    System.out.println("Voltando para o menu de usuários...");
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+
+
+            }
+
+
+
+        }
     }
 
 }
